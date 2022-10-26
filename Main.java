e6bd5bc5e0320dcdc013b24d24fd5689f753c638
hkrishg <109535284+hkrishg@users.noreply.github.com>
2022-10-26 16:44:22 +0000

    another PS

--- Main.java
+++ Main.java
@@ -16,6 +16,7 @@ class Main {
   // Given a binary tree, populate an array to represent the averages of all of
   // its levels.
 
+  private static List<Double> leverOrderTraversal(TreeNode root) {
     List<Double> res = new ArrayList<>();
     if (root == null)
       return res;
@@ -39,6 +40,34 @@ class Main {
     return res;
   }
 
+
+  // Problem 1: Find the largest value on each level of a binary tree.
+
+  
+  private static List<Integer> LargestValueEachLevel(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    if (root == null)
+      return res;
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      int maxvalue = 0;
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+
+        TreeNode currentNode = queue.poll();
+        maxvalue = Math.max(maxvalue, currentNode.val);
+        // insert the children of current node to the queue
+        if (currentNode.left != null)
+          queue.add(currentNode.left);
+        if (currentNode.right != null)
+          queue.add(currentNode.right);
+      }
+      res.add(maxvalue);
+    }
+    return res;
+  }
+
   public static void main(String[] args) {
     TreeNode root = new TreeNode(12);
     root.left = new TreeNode(7);
@@ -48,5 +77,7 @@ class Main {
     root.right.right = new TreeNode(5);
 
     System.out.println(Main.leverOrderTraversal(root));
+    System.out.println(Main.LargestValueEachLevel(root));
+
   }
 }
